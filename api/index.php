f244c713f8e0b7e67c909437188cddab7c63f305
andersonyeakey <andersonyeakey@gmail.com>
2026-08-06 14:07:39 -0400

    Added text and interactability to orbiters.
    Added URLs and self-righting text to three orbiting elements.

--- api/index.php
+++ api/index.php
@@ -10,22 +10,27 @@
     </head>
 
     <body style="height:100%; display:flex">
-        <!--<a href="/about">About</a>
-        <a href="/projects">Projects</a>
-        <a href="/demos">Demos</a>-->
+        <img id="profile-img" class="floating interactive" src="../public/mainprofile.jpg" alt="Anderson Yeakey's Profile Picture" fetchpriority="high" style="top: 50%; left: 50%; width:30vh; border-radius: 50%; border: 4px solid black;">
 
-        <img class="floating" src="../public/mainprofile.jpg" alt="Anderson Yeakey's Profile Picture" fetchpriority="high" style="top: 50%; left: 50%; width:30vh; border-radius: 50%; border: 4px solid black;">
-
-        <svg id="left-item" class="floating orbiting" height="100" width="100">
-            <circle r="45" cx="50" cy="50" fill="darkblue"/>
+        <svg id="left-item" class="floating orbiting" height="16vh" width="16vh">
+            <a href="/projects">
+                <circle class="interactive" r="5vh" cx="8vh" cy="8vh" fill="darkblue"/>
+                <text dominant-baseline="middle" text-anchor="middle" font-size="1.5em" x="8vh" y="8vh" fill="white">Projects</text>
+            </a>
         </svg>
 
-        <svg id="right-item" class="floating orbiting" height="100" width="100">
-            <circle r="45" cx="50" cy="50" fill="darkblue"/>
+        <svg id="right-item" class="floating orbiting" height="16vh" width="16vh">
+            <a href="/demos">
+                <circle class="interactive" r="5vh" cx="8vh" cy="8vh" fill="darkblue"/>
+                <text dominant-baseline="middle" text-anchor="middle" font-size="1.5em" x="8vh" y="8vh" fill="white">Demos</text>
+            </a>
         </svg>
 
-        <svg id="bottom-item" class="floating orbiting" height="100" width="100">
-            <circle r="45" cx="50" cy="50" fill="darkblue"/>
+        <svg id="bottom-item" class="floating orbiting" height="16vh" width="16vh">
+            <a href="/about">
+                <circle class="interactive" r="5vh" cx="8vh" cy="8vh" fill="darkblue"/>
+                <text dominant-baseline="middle" text-anchor="middle" font-size="1.5em" x="8vh" y="8vh" fill="white">About</text>
+            </a>
         </svg>
     </body>
 
