7f947235a7cde2a1ccccab53321f2a165d99812e
andersonyeakey <andersonyeakey@gmail.com>
2026-08-06 15:32:38 -0400

    Added custom fonts and text.
    Added JetBrains Mono and Basically A Mono fonts to the project, and added text to the orbiters as well as a header name.

--- api/index.php
+++ api/index.php
@@ -6,32 +6,37 @@
 <html style="height:100%">
     <head>
         <title>Home</title>
+        <link rel="stylesheet" href="/public/main.css">
         <link rel="stylesheet" href="/public/index.css">
     </head>
 
-    <body style="height:100%; display:flex">
-        <img id="profile-img" class="floating interactive" src="../public/mainprofile.jpg" alt="Anderson Yeakey's Profile Picture" fetchpriority="high" style="top: 50%; left: 50%; width:30vh; border-radius: 50%; border: 4px solid black;">
+    <body>
+        <main>
+            <h1 style="font-size:6em;">Anderson Yeakey</h1>
 
-        <svg id="left-item" class="floating orbiting" height="16vh" width="16vh">
-            <a href="/projects">
-                <circle class="interactive" r="5vh" cx="8vh" cy="8vh" fill="darkblue"/>
-                <text dominant-baseline="middle" text-anchor="middle" font-size="1.5em" x="8vh" y="8vh" fill="white">Projects</text>
-            </a>
-        </svg>
+            <img id="profile-img" class="floating interactive" src="../public/mainprofile.jpg" alt="Anderson Yeakey's Profile Picture" fetchpriority="high">
 
-        <svg id="right-item" class="floating orbiting" height="16vh" width="16vh">
-            <a href="/demos">
-                <circle class="interactive" r="5vh" cx="8vh" cy="8vh" fill="darkblue"/>
-                <text dominant-baseline="middle" text-anchor="middle" font-size="1.5em" x="8vh" y="8vh" fill="white">Demos</text>
-            </a>
-        </svg>
+            <svg id="left-item" class="floating orbiting" height="16vh" width="16vh">
+                <a href="/projects">
+                    <circle class="interactive" r="5vh" cx="8vh" cy="8vh" fill="darkblue"/>
+                    <text dominant-baseline="middle" text-anchor="middle" font-size="1.5em" x="8vh" y="8vh" fill="white">Projects</text>
+                </a>
+            </svg>
 
-        <svg id="bottom-item" class="floating orbiting" height="16vh" width="16vh">
-            <a href="/about">
-                <circle class="interactive" r="5vh" cx="8vh" cy="8vh" fill="darkblue"/>
-                <text dominant-baseline="middle" text-anchor="middle" font-size="1.5em" x="8vh" y="8vh" fill="white">About</text>
-            </a>
-        </svg>
+            <svg id="right-item" class="floating orbiting" height="16vh" width="16vh">
+                <a href="/demos">
+                    <circle class="interactive" r="5vh" cx="8vh" cy="8vh" fill="darkblue"/>
+                    <text dominant-baseline="middle" text-anchor="middle" font-size="1.5em" x="8vh" y="8vh" fill="white">Demos</text>
+                </a>
+            </svg>
+
+            <svg id="bottom-item" class="floating orbiting" height="16vh" width="16vh">
+                <a href="/about">
+                    <circle class="interactive" r="5vh" cx="8vh" cy="8vh" fill="darkblue"/>
+                    <text dominant-baseline="middle" text-anchor="middle" font-size="1.5em" x="8vh" y="8vh" fill="white">About</text>
+                </a>
+            </svg>
+        </main>
     </body>
 
     <script>
