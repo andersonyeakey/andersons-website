67281e1f8825e719c5d9a6901a09fb48730c9c8c
andersonyeakey <andersonyeakey@gmail.com>
2026-08-06 12:53:43 -0400

    Reimplemented orbits with rotation.
    Scrapped the old orbit system and added a new (and simpler) one with rotations, with the only JS being a calculation for aspect ratio since it spits out a real number and not something with pixel units (so it can be plugged into acos()).

--- api/index.php
+++ api/index.php
@@ -16,35 +16,26 @@
 
         <img class="floating" src="../public/mainprofile.jpg" alt="Anderson Yeakey's Profile Picture" fetchpriority="high" style="top: 50%; left: 50%; width:30vh; border-radius: 50%; border: 4px solid black;">
 
-        <svg id="left-item" class="floating" height="100" width="100">
+        <svg id="left-item" class="floating orbiting" height="100" width="100">
             <circle r="45" cx="50" cy="50" fill="darkblue"/>
         </svg>
 
-        <svg id="right-item" class="floating" height="100" width="100">
+        <svg id="right-item" class="floating orbiting" height="100" width="100">
             <circle r="45" cx="50" cy="50" fill="darkblue"/>
         </svg>
 
-        <svg id="bottom-item" class="floating" height="100" width="100">
+        <svg id="bottom-item" class="floating orbiting" height="100" width="100">
             <circle r="45" cx="50" cy="50" fill="darkblue"/>
         </svg>
     </body>
 
     <script>
-        //TODO: Rework entire orbit system to be rotation transforms in CSS. This is too much
-
-        var orbitRadius = Number(getComputedStyle(document.documentElement).getPropertyValue('--orbit-radius').slice(0, -2)) / 100;
-        var orbitPadding = Number(getComputedStyle(document.documentElement).getPropertyValue('--orbit-padding').slice(0, -2)) / 100;
-
         window.onload = function() {
-            var topPadding = Math.min(orbitRadius * window.innerHeight, 0.5 * window.innerHeight - Math.sqrt((orbitRadius * window.innerHeight + orbitPadding * window.innerWidth) * (orbitRadius * window.innerHeight - orbitPadding * window.innerWidth)));
-            document.getElementById("left-item").style.top = topPadding + "px";
-            document.getElementById("right-item").style.top = topPadding + "px";
+            document.documentElement.style.setProperty("--aspect-ratio", window.innerWidth / window.innerHeight);
         }
 
         addEventListener("resize", (event) => {
-            var topPadding = Math.min(orbitRadius * window.innerHeight, 0.5 * window.innerHeight - Math.sqrt((orbitRadius * window.innerHeight + orbitPadding * window.innerWidth) * (orbitRadius * window.innerHeight - orbitPadding * window.innerWidth)));
-            document.getElementById("left-item").style.top = topPadding + "px";
-            document.getElementById("right-item").style.top = topPadding + "px";
+            document.documentElement.style.setProperty("--aspect-ratio", window.innerWidth / window.innerHeight);
         })
     </script>
 </html>
