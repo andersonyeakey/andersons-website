8eabd07296fa8ce0d6a607edc916778981b979cf
andersonyeakey <andersonyeakey@gmail.com>
2026-08-07 13:16:15 -0400

    Added under construction banner to demos page.

--- api/demos.php
+++ api/demos.php
@@ -3,16 +3,23 @@
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="en">
     <head>
         <title>Demos</title>
+        <link rel="stylesheet" href="/public/main.css">
+        <link rel="stylesheet" href="/public/demos.css">
     </head>
 
     <body>
-        <a href="/">Home</a>
+        <!--<a href="/">Home</a>
         <a href="/about">About</a>
-        <a href="/projects">Projects</a>
+        <a href="/projects">Projects</a>-->
 
-        <p>Under construction</p>
+        <h1>This page is under construction!</h1>
+        <a href="javascript:history.back()">Return</a>
     </body>
 </html>
+
+<?php
+
+?>
