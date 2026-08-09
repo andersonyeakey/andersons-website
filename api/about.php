3c438a52ac02285d7ed5afaf7ccdad9a55deea13
andersonyeakey <andersonyeakey@gmail.com>
2026-08-08 22:10:59 -0400

    Initial static setup.

--- api/about.php
+++ api/about.php
@@ -6,16 +6,131 @@
 <html>
     <head>
         <title>About</title>
+        <link rel="stylesheet" href="/public/main.css">
+        <link rel="stylesheet" href="/public/about.css">
     </head>
 
     <body>
-        <a href="/">Home</a>
+        <!--<a href="/">Home</a>
         <a href="/projects">Projects</a>
         <a href="/demos">Demos</a>
 
         <a href="mailto:andersonyeakey@gmail.com">andersonyeakey@gmail.com</a>
-        <a href="https://www.linkedin.com/in/anderson-yeakey/">LinkedIn</a>
+        <a href="https://www.linkedin.com/in/anderson-yeakey/">LinkedIn</a>-->
 
-        <p>Under construction</p>
+        <h1>About Me</h1>
+
+        <main>
+            <section>
+                <p>A little paragraph and/or snippet about myself.</p>
+            </section>
+
+            <img id="secondary-profile-img" src="../public/mainprofile.jpg" alt="Anderson Yeakey's Profile Picture">
+
+            <section>
+                <h2>Computer Languages</h2>
+
+                <hr>
+
+                <h3>Proficient:</h3>
+                <ul>
+                    <li>C</li>
+                    <li>CSS</li>
+                    <li>HTML</li>
+                    <li>JavaScript</li>
+                    <li>PHP</li>
+                    <li>Python</li>
+                    <li>SQL</li>
+                </ul>
+
+                <hr>
+
+                <h3>Learning:</h3>
+                <ul>
+                    <li>C+</li>
+                    <li>C#</li>
+                    <li>Markdown</li>
+                </ul>
+
+                <hr>
+
+                <h3>Have Used:</h3>
+                <ul>
+                    <li>Java</li>
+                    <li>32-Bit MIPS Assembly</li>
+                    <li>Typescript</li>
+                </ul>
+            </section>
+
+            <section>
+                <h2>Dev Frameworks & Tools</h2>
+
+                <hr>
+
+                <h3>Proficient:</h3>
+                <ul>
+                    <li>Git</li>
+                    <li>PostreSQL</li>
+                    <li>TailwindCSS</li>
+                </ul>
+
+                <hr>
+
+                <h3>Learning:</h3>
+                <ul>
+                    <li>ASP.NET Core</li>
+                    <li>Node.js</li>
+                </ul>
+
+                <hr>
+
+                <h3>Have Used:</h3>
+                <ul>
+                    <li>Express</li>
+                    <li>Jest</li>
+                    <li>Vitest</li>
+                    <li>Vue</li>
+                </ul>
+            </section>
+
+            <section>
+                <h2>Human Languages</h2>
+
+                <hr>
+
+                <h3>Fluent:</h3>
+                <ul>
+                    <li>English</li>
+                </ul>
+
+                <hr>
+
+                <h3>Learning:</h3>
+                <ul>
+                    <li>American Sign Language</li>
+                    <li>German</li>
+                </ul>
+            </section>
+        </main>
+
+        <footer>
+            <div>
+                <!--<svg></svg>-->
+                <p>Email</p>
+                <a href="mailto:andersonyeakey@gmail.com">andersonyeakey@gmail.com</a>
+            </div>
+
+            <div>
+                <!--<svg></svg>-->
+                <p>GitHub</p>
+                <a href="https://github.com/andersonyeakey">andersonyeakey</a>
+            </div>
+
+            <div>
+                <!--<svg></svg>-->
+                <p>LinkedIn</p>
+                <a href="https://www.linkedin.com/in/anderson-yeakey/">Anderson Yeakey</a>
+            </div>
+        </footer>
     </body>
 </html>
